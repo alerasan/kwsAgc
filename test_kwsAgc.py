176b51966ad87612d6f81017f4087bee279b3a83
alerasan <alloys.109@gmail.com>
2021-12-06 04:38:47 +0300

    added repeats and time taken printing

--- test_kwsAgc.py
+++ test_kwsAgc.py
@@ -1,4 +1,5 @@
 import numpy as np
+import time
 from argparse import ArgumentParser, Namespace
 from kws_agc import KwsAgc, KwsAgcParams
 
@@ -12,6 +13,9 @@ def get_args() -> Namespace:
 
     parser.add_argument("-t","--test_file", dest="test_file", type=str, required=True,
                         help="Path to test audio file")
+    
+    parser.add_argument("-r","--repeats", dest="repeats", type=int, required=True,
+                        help="amount of repeats")
 
     return parser.parse_args()
 
@@ -23,8 +27,11 @@ agc = KwsAgc(KwsAgcParams())
 with open(args.input_file, 'rb') as h:
     _pcm = np.fromfile(h, dtype=np.int16)
 
-out_pcm = agc(_pcm, rate=16000)
-# out_pcm.astype('int16').tofile('data/agc_test_float.res.raw')
+start = time.time()
+for r in range(args.repeats):
+    out_pcm = agc(_pcm, rate=16000)
+end = time.time()
+print("time taken: " + str(end - start))
 
 with open(args.test_file, 'rb') as h:
     _pcm_origin = np.fromfile(h, dtype=np.int16)
